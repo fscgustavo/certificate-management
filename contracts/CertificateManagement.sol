a7372ef2d3f3f4011d49a73ced01a2b6e26a5bb4
Gustavo Fonseca <gus.fonnseca@gmail.com>
2022-08-29 23:12:55 -0300

    create the roles

---
+++ contracts/CertificateManagement.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract CertificateManagement is AccessControl {
+    bytes32 public constant ORGANIZATION_ROLE = keccak256("ORGANIZATION_ROLE")
+    bytes32 public constant UNIVERSITY_ROLE = keccak256("UNIVERSITY_ROLE")
+    bytes32 public constant CERTIFIER_ROLE = keccak256("CERTIFIER_ROLE")
+
+    constructor() {
+        _setRoleAdmin(UNIVERSITY_ROLE, ORGANIZATION_ROLE)
+        _setRoleAdmin(CERTIFIER_ROLE, UNIVERSITY_ROLE)
+
+        _grantRole(ORGANIZATION_ROLE, msg.sender)
+    }
+}
