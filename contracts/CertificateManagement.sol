128e4484e258e1e35f7227ba46c81d095124374a
Gustavo Fonseca <gus.fonnseca@gmail.com>
2022-09-04 19:47:49 -0300

    getter functions and initial idea of certificate emission

--- contracts/CertificateManagement.sol
+++ contracts/CertificateManagement.sol
@@ -5,13 +5,32 @@ import '@openzeppelin/contracts/access/AccessControl.sol';
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract CertificateManagement is AccessControl, ERC20 {
+    enum CertificateStatus {
+        Invalid,
+        Valid
+    }
+
+    struct Certifier {
+        address certifier;
+        address university;
+        string informationURI;
+    }
+
+    struct Certificate {
+        Certifier issuer;
+        CertificateStatus status;
+        uint256 issueDate;
+    }
+
     bytes32 public constant ORGANIZATION_ROLE = keccak256('ORGANIZATION_ROLE');
     bytes32 public constant UNIVERSITY_ROLE = keccak256('UNIVERSITY_ROLE');
     bytes32 public constant CERTIFIER_ROLE = keccak256('CERTIFIER_ROLE');
-
     uint256 public constant MAX_ALLOWANCE = 2**256 - 1;
 
-    mapping(address => address) private _certifierToUniversity;
+    mapping(address => address) private s_certifierToUniversity;
+    mapping(bytes32 => Certificate) private s_certificates;
+    mapping(bytes32 => string) private s_revocationReason;
+    mapping(address => string) private s_universityURI;
 
     constructor() ERC20('CToken', 'CTK') {
         _setRoleAdmin(UNIVERSITY_ROLE, ORGANIZATION_ROLE);
@@ -20,15 +39,16 @@ contract CertificateManagement is AccessControl, ERC20 {
         _grantRole(ORGANIZATION_ROLE, msg.sender);
     }
 
-    function addUniversity(address account)
+    function addUniversity(address account, string memory universityURI)
         external
         onlyRole(ORGANIZATION_ROLE)
     {
         _grantRole(UNIVERSITY_ROLE, account);
+        s_universityURI[account] = universityURI;
     }
 
     function addCertifier(address account) external onlyRole(UNIVERSITY_ROLE) {
-        _certifierToUniversity[account] = msg.sender;
+        s_certifierToUniversity[account] = msg.sender;
         _grantRole(CERTIFIER_ROLE, account);
 
         approve(account, MAX_ALLOWANCE);
@@ -45,9 +65,54 @@ contract CertificateManagement is AccessControl, ERC20 {
         external
         onlyRole(UNIVERSITY_ROLE)
     {
-        delete _certifierToUniversity[account];
+        delete s_certifierToUniversity[account];
         _revokeRole(CERTIFIER_ROLE, account);
 
         approve(account, 0);
+    }
+
+    function registerCertificate(bytes32 certificateId, uint256 issueDate)
+        external
+        onlyRole(CERTIFIER_ROLE)
+    {
+        address university = s_certifierToUniversity[msg.sender];
+
+        s_certificates[certificateId] = Certificate(
+            Certifier(msg.sender, university, s_universityURI[university]),
+            CertificateStatus.Valid,
+            issueDate
+        );
+    }
+
+    function getCertificate(bytes32 certificateId)
+        external
+        view
+        returns (Certificate memory)
+    {
+        return s_certificates[certificateId];
+    }
+
+    function getRevocationReason(bytes32 certificateId)
+        external
+        view
+        returns (string memory)
+    {
+        return s_revocationReason[certificateId];
+    }
+
+    function getUniversityOfCertifier(address certifier)
+        external
+        view
+        returns (address)
+    {
+        return s_certifierToUniversity[certifier];
+    }
+
+    function getUniversityURI(address university)
+        external
+        view
+        returns (string memory)
+    {
+        return s_universityURI[university];
     }
 }
