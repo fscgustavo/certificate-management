d4e5c18868731616228f8b4c4710c3e53b808a36
Gustavo Fonseca <gus.fonnseca@gmail.com>
2022-09-11 10:33:09 -0300

    remove unused properties of helper-hardhat.config

--- helper-hardhat.config.ts
+++ helper-hardhat.config.ts
@@ -14,26 +14,13 @@ export interface networkConfigItem {
 export const networkConfig: Record<number, networkConfigItem> = {
   31337: {
     name: 'localhost',
-    subscriptionId: '588',
-    gasLane:
-      '0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc', // 30 gwei
-    keepersUpdateInterval: '30',
-    raffleEntranceFee: '100000000000000000', // 0.1 ETH
-    callbackGasLimit: '500000', // 500,000 gas
-  },
-  4: {
-    name: 'rinkeby',
-    subscriptionId: '588',
-    gasLane:
-      '0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc', // 30 gwei
-    keepersUpdateInterval: '30',
-    raffleEntranceFee: '100000000000000000', // 0.1 ETH
-    callbackGasLimit: '500000', // 500,000 gas
-    blockConfirmations: 4,
   },
   1: {
     name: 'mainnet',
-    keepersUpdateInterval: '30',
+  },
+  4: {
+    name: 'rinkeby',
+    blockConfirmations: 4,
   },
 };
 
